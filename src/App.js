9e41a0e6a5ae44ababee9f52ef1890450b97338c
N Gulshan Kumar <84889171+gulshan0201@users.noreply.github.com>
2022-10-28 18:32:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Aboutus from './Components/Aboutus';
+
+function App() {
+  return (
+    <div className="App">
+      <Aboutus />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Aboutus.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Aboutus = () => {
+  return (
+    <>
+      <div className='container'>
+        <h1>About us</h1>
+        <p>At Zappos.com, our purpose is simple: to live and deliver WOW.</p>
+        <p>Twenty years ago, we began as a small online retailer that only sold shoes. Today, we still sell
+          shoes —— as well as cloathing, handbags, accessories, and more. That "more" is providing the very best customer service
+          customer experience, and company culture. We aim to inspire the world by showing its possible to simultaneously deliver happiness
+          to customers, employees, vendors, shareholders, and the community in a long-term, sustainable way.
+          <br />
+          <br />
+          We hope that in the future people won't even realise we started selling shoes online. insted, they'll know Zappos as a
+          service company that just happens to sell _______.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default Aboutus
